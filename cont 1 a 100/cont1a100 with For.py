eb4aea9f8aa98a6971a34029bd891c06eb636b9e
William Rodrigues de Sousa <rm78369@fiap.com.br>
2019-02-12 17:05:24 -0200

    Comment added

--- cont 1 a 100/cont1a100 with For.py
+++ cont 1 a 100/cont1a100 with For.py
@@ -11,3 +11,12 @@ for n in range(100):
     else:
         print(n)
         
+"""
+Diferente de outras linguagens que conheci, no Python o for funciona
+de uma forma diferente (porém a ideia é a mesma e, até mais simples)
+Em vez de fazer um simples for(n==0;n<=100;n++)
+Se faz for n in range(100)
+A ideia é a mesma, mesmo que escrita de forma diferente
+Quando coloca um in range(100), simplesmente explica ao Python que deseja
+que a função aumente a variável n até chegar em 100, mais prático!
+"""
